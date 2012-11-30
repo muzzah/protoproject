d879ed5c59250540aca312989fe412e8e376b26b
Umar <umar_umar_umar_umar@hotmail.com>
2012-11-30 13:59:50 +0000

    Keep the slider value in place at all times and update it via JS

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,7 @@
 $(function() {
 	//Document ready
 	radio('Audio:filter1').subscribe(function(msg) {
-		$('#rangeinput').val(msg.value);
+		$('#rangeinput, #rangevalue').val(msg.value);
 		//Move the sliders around
 	});
 });
