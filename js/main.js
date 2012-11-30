c97b5456cae4115c103dd91f6fc127da2e0df2ff
Mustafa <msezgin@thoughtworks.com>
2012-11-30 11:25:01 -0500

    Started the connection peer stuff but commented out

--- js/main.js
+++ js/main.js
@@ -1,7 +1,39 @@
+var signallingChannel;
+var settings = {
+    apikey:'455f08c6-2ee9-4f75-819c-1229744bfda8',
+    channel:'video'
+};
+
 $(function() {
     // Handler for .ready() called.
     // Listen for things on the "info" channel
     radio("info").subscribe(function (msg) { console.info('info', msg); });
-    bringUpVideo();
+    setupPubSub();
+
+    $("#startbutton").click(startClicked);
 });
+
+function setupPubSub(){
+    signallingChannel = new jXSockets.WebSocket("ws://xsocketslive.cloudapp.net:10101/XSockets.Live.Realtime.API",
+        "XSockets.Live.Realtime.API", settings);
+
+    // Fires when a connection is established
+    signallingChannel.bind(XSockets.Events.open, function (message) {
+        console.log("open to xsockets");
+    });
+
+// Fires when a connection is closed by the server
+    signallingChannel.bind(XSockets.Events.close, function (message) {
+        console.log("close to xsockets");
+    });
+
+
+// Fires when a error is sent from the server
+    signallingChannel.bind(XSockets.Events.onError, function (error) {
+        console.log("Error on xsockets ", error);
+    });
+
+    // Subscribe to "foo"
+    signallingChannel.bind(settings.channel, processSignal);
+}
 
--- js/video.js
+++ js/video.js
@@ -1,33 +1,82 @@
+
+
 function hasGetUserMedia() {
-    // Note: Opera is unprefixed.
     return navigator.webkitGetUserMedia;
 }
 
-var onFailSoHard = function(e) {
+var alertUserDoesntHaveWebRTCSupport = function (e) {
     alert('getUserMedia() is not supported in your browser');
 };
 
-function bringUpVideo(){
+function startClicked() {
+        console.log("start clicked");
 
-    if (hasGetUserMedia()) {
+    navigator.webkitGetUserMedia({ "audio":false, "video":true }, function (localMediaStream) {
 
-        // Not showing vendor prefixes.
-        navigator.webkitGetUserMedia({video: true, audio: false}, function(localMediaStream) {
-            var video = document.querySelector('video');
-            video.src = window.URL.createObjectURL(localMediaStream);
+        var video = $('#myvideo');
+        var myVideoStream = window.URL.createObjectURL(localMediaStream);
+        video.attr('src', myVideoStream);
 
-            // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
-            // See crbug.com/110938.
-            video.onloadedmetadata = function(e) {
-                // Ready to go. Do some stuff.
-            };
-        }, onFailSoHard);
+    });
 
-    } else {
-        onFailSoHard();
-    }
+
+
+
 }
 
+
+function processSignal(message) {
+    console.log("message received", message);
+}
+
+
+function setupPeerConnection(){
+//            pc = new RTCPeerConnection(null);
+//
+//        // send any ice candidates to the other peer
+//        pc.onicecandidate = function (evt) {
+//            signalingChannel.trigger(settings.channel, {
+//                message:JSON.stringify({ "candidate":evt.candidate })
+//            });
+//        };
+//
+//        // once remote stream arrives, show it in the remote video element
+//        pc.onaddstream = function (evt) {
+//            //remoteView.src = URL.createObjectURL(evt.stream);
+//        };
+//
+//
+//        // get the local stream, show it in the local video element and send it
+//        navigator.getUserMedia({ "audio":true, "video":true }, function (localMediaStream) {
+//
+//            var video = $('#myvideo');
+//            var myVideoStream = window.URL.createObjectURL(localMediaStream);
+//            video.attr('src', myVideoStream);
+//            pc.addStream(stream);
+//
+//            if (isCaller)
+//                pc.createOffer(gotDescription);
+//            else
+//                pc.createAnswer(pc.remoteDescription, gotDescription);
+//
+//            function gotDescription(desc) {
+//                pc.setLocalDescription(desc);
+//                signalingChannel.send(JSON.stringify({ "sdp":desc }));
+//            }
+//        });
+//    }
+//
+//    signalingChannel.onmessage = function (evt) {
+//        if (!pc)
+//            start(false);
+//
+//        var signal = JSON.parse(evt.data);
+//        if (signal.sdp)
+//            pc.setRemoteDescription(new RTCSessionDescription(signal.sdp));
+//        else
+//            pc.addIceCandidate(new RTCIceCandidate(signal.candidate));
+//    };
+}
 
 
 
