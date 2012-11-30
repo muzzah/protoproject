30673e9cf6bb1beaba29a648f8254276655fb9ae
Umar <umar_umar_umar_umar@hotmail.com>
2012-11-30 14:35:52 +0000

    Slider controls

--- js/slider.js
+++ js/slider.js
@@ -3,7 +3,12 @@ $(function() {
 
 	radio('Audio:filter1').subscribe(function(msg) {
 		$('#slider1').find('input').val(msg.value);
+		$('#slider1').find('.value').text(msg.value)
 		//Move the sliders around
+	});
+
+	$('.slider input').on('change', function(val) {
+		$(this).parent('.slider').find('.value').text($(this).val());
 	});
 
 });
